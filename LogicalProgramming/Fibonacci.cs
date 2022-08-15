7e60c30b9bc17a162b0eab8d164e5dfddeee1ff7
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-15 19:22:25 +0530

    [Darshan]: Add Fibonacci number program

---
+++ LogicalProgramming/Fibonacci.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class Fibonacci
+    {
+        //intial values for fibonacci
+        private int firstPreviousValue = 0;
+        private int secondPreviousValue = 1;
+        private int fibonacci;
+
+
+
+        public void GetFibonacciNum()
+        {
+            Console.WriteLine("Enter the n th no. for which you want fibonacci  number: ");
+            int n = int.Parse(Console.ReadLine());
+
+
+            if (n == 0)
+                Console.WriteLine("Fibonacci no. is: " + "Zero");
+
+            else if (n == 1)
+                Console.WriteLine("Fibonacci no. is: " + 1);
+
+            else
+            {
+                for (var i = 1; i < n; i++)
+                {
+                    fibonacci = firstPreviousValue + secondPreviousValue;
+                    firstPreviousValue = secondPreviousValue;
+                    secondPreviousValue = fibonacci;
+
+
+
+                }
+
+                Console.WriteLine("Fibonacci no. is: " + fibonacci);
+            }
+        }
+    }
+}
---
+++ LogicalProgramming/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LogicalProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Programs");
+            Console.WriteLine("Enter the number from below choices to execute a program");
+            Console.WriteLine("1. Fibonacci Number");
+
+            int ans = int.Parse(Console.ReadLine());
+
+            switch (ans)
+            {
+
+                case 1:
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.GetFibonacciNum();
+                    break;
+                default:
+                    Console.WriteLine("enter the number from given choices");
+                    break;
+            }
+        }
+    }
+}
